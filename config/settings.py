d55d8440c572c57f9e924bc2f675a27ef0b75a49
Sardor Irgashev <irgashevsardor99@gmail.com>
2021-11-25 21:20:51 +0500

    Edit settings.py

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if os.path.isfile(os.path.join(BASE_DIR, ".env")):
     load_dotenv(os.path.join(BASE_DIR, ".env"))
